b76bf4adf9dd0d0c9aa71d34be3dc46faca14404
孙昊翔 <673686754@qq.com>
2017-02-16 12:56:55 +0800

    fooddetail

--- src/components/foodDetail/foodDetail.vue
+++ src/components/foodDetail/foodDetail.vue
@@ -128,9 +128,7 @@
         if (!this.detailWrapper) {
           this.detailWrapper = new BScroll(this.$refs.detailWrapper, {
             click: true
-          });
-        } else {
-          this.detailWrapper.refresh()
+          })
         }
       },
       addCart(event) {
